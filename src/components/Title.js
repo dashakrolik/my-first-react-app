7d423d54fb6b20b265d2eda481d5f511d2eda4b2
DASHA <dariaepaneshnikova@icloud.com>
2019-02-25 14:33:28 +0100

    added component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+  render() {
+    return 'test'
+  }
+}
+
+export default class Title extends Component {
+  render() {
+    return 'test'
+  }
+}
